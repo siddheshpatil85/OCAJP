8601ea400065779fe51f335ab3febd547e95763a
Siddhesh Patil <SiddheshPatil85@gmail.com>
2019-09-18 10:14:08 +0530

    1st OCAJP commit - test GitHub

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
-    String strTest = "Hello World";
+    String strTest ;
+
+    HelloWorld(){
+        System.out.println("In Defined constructor");
+    }
 
     public static void main (String args[]){
         HelloWorld hw = new HelloWorld();
+        hw.strTest = "Initialised Instance Variable from Main";
         System.out.println("Print Instance Variable strTest: " + hw.strTest);
     }
 }

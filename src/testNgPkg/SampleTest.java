9c8e613448897d7ba0c1689e63f4f5c89d252074
Siddhesh Patil <SiddheshPatil85@gmail.com>
2019-09-18 18:39:29 +0530

    1st OCAJP commit - Added JUnit Test 2nd OCAJP commit - Added TestNG

--- src/testNgPkg/SampleTest.java
+++ src/testNgPkg/SampleTest.java
@@ -1,10 +1,31 @@
 package testNgPkg;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class SampleTest {
+    @BeforeSuite
+    public void bfrSuite ()
+    {
+        System.out.println("Executing BeforeSuite");
+    }
 
+    @BeforeTest
+    public void bfrTest ()
+    {
+        System.out.println("Executing bfrTest");
+    }
+    @BeforeClass
+    public void bfrClass ()
+    {
+        System.out.println("Executing bfrClass");
+    }
+
+    @BeforeMethod
+    public void bfrMethod ()
+    {
+        System.out.println("Executing bfrMethod");
+    }
 
         @Test(priority = 2)
         public void testMethodTwo ()
@@ -12,15 +33,40 @@ public class SampleTest {
             System.out.println("Executing testMethodTwo");
             Assert.assertTrue(2 == 2);
         }
+
         @Test(priority = 1)
         public void testMethodOne ()  {
         System.out.println("Executing testMethodOne");
         Assert.assertTrue("A" == "A");
         }
+
         @Test(dependsOnMethods = {"testMethodTwo"})
         public void testMethodThree () {
-            System.out.println("Executing testMethodThree");
-            Assert.assertTrue(true);
-        }
+        System.out.println("Executing testMethodThree");
+        Assert.assertTrue(true);
+        Assert.assertTrue(false);
+    }
+
+    @AfterSuite
+    public void afterSuite ()
+    {
+        System.out.println("Executing afterSuite");
+    }
+    @AfterTest
+    public void afterTest ()
+    {
+        System.out.println("Executing afterTest");
+    }
+    @AfterClass
+    public void afterClass ()
+    {
+        System.out.println("Executing afterClass");
+    }
+
+    @AfterMethod
+    public void afterMethod ()
+    {
+        System.out.println("Executing afterMethod");
+    }
 
 }

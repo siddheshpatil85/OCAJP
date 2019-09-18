5a495fd44fde39227ac3e6b95668cee29edc78b5
Siddhesh Patil <SiddheshPatil85@gmail.com>
2019-09-18 09:59:58 +0530

    1st OCAJP commit - test GitHub

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    String strTest = "Hello World";
+
+    public static void main (String args[]){
+        HelloWorld hw = new HelloWorld();
+        System.out.println("Print Instance Variable strTest: " + hw.strTest);
+    }
+}
